bad5864cbac055ac5eefd5f0b7706ab5fc11ab23
Nikawali <x3107776126@163.com>
2026-06-21 21:46:24 +0800

    null

---
+++ src/main/java/org/example/classpai/ClassPaiApplication.java
@@ -0,0 +1,13 @@
+package org.example.classpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassPaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassPaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/classpai/ClassPaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.classpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassPaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
